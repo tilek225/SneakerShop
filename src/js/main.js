940cdd985f3fedcd3a4c14ee08f98f219b9de892
Tilek <askaralievtilek@gmail.com>
2022-03-29 21:35:42 +0600

    add styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
+let swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+        delay: 1000,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+    },
+});
+
 document.addEventListener('DOMContentLoaded', function (e) {
     let row = document.querySelector('.main__row');
 
@@ -6,6 +16,9 @@ document.addEventListener('DOMContentLoaded', function (e) {
         .then(data => data.forEach(item => {
             row.innerHTML += `
                 <div class="main__card">
+                    <button type="button" class="main__card-favorites">
+                    ❤	
+                    </button>
                     <img class="main__card-img" src="${item.imageUrl}" alt="${item.title}">
                     <h3 class="main__card-title">${item.title}</h3>
                     <div class="main__card-pay">
@@ -15,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
                         </div>
                         <button class="main__card-btn">
                             <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#000000"/>
+                            <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#A5D364"/>
                             </svg>
                         </button>
                     </div>
